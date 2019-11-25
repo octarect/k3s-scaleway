0fd6499474cb5e708ce70d70ee241a028fda19bb
Ryota Kota <ryota.kota@member.fsf.org>
2019-11-25 23:53:53 +0900

    current k3s (v1.0.0) don't have ExternalIP unless --node-external-ip is passed

--- files/k3s_agent.sh
+++ files/k3s_agent.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
-export INSTALL_K3S_EXEC="agent --server ${server_url} --cluster-secret ${cluster_secret}"
+# Get public IP
+public_ip=$(curl -s inet-ip.info)
+
+export INSTALL_K3S_EXEC="agent --server ${server_url} --cluster-secret ${cluster_secret} --node-external-ip $public_ip"
 
 if [ `command -v curl` ]; then
   curl -sfL https://get.k3s.io | sh -s -
--- files/k3s_server.sh
+++ files/k3s_server.sh
@@ -3,7 +3,7 @@
 # Get public IP
 public_ip=$(curl -s inet-ip.info)
 
-export INSTALL_K3S_EXEC="--cluster-secret ${cluster_secret} --advertise-address $public_ip"
+export INSTALL_K3S_EXEC="--cluster-secret ${cluster_secret} --advertise-address $public_ip --node-external-ip $public_ip"
 
 if [ `command -v curl` ]; then
   curl -sfL https://get.k3s.io | sh -s -
